da26ead0f1ad042a5e401752cf6dc90358ea2824
bogdan.vlad <bogdan.vlad@grabtaxi.com>
2023-05-20 17:19:25 +0300

    add inference with tiles

--- sisr.py
+++ sisr.py
@@ -14,6 +14,10 @@ DEVICE = torch.device("cuda" if torch.cuda.is_available() else "cpu")  # device 
 ARCHITECTURES_DIR = "./sisr_architectures/"  # directory with architecture classes
 MODEL_WEIGHTS = "./sisr_weights/"  # directory with model weights
 
+# default tile parameters
+TILE_SIZE = 400
+TILE_OVERLAP = 32
+
 # model name to the class needed for that model
 NAME_2_CLASS = {
     "swin_ir": SwinIR,
@@ -51,15 +55,21 @@ NAME_2_ARGS = {
 
 
 @torch.no_grad()
-def run_sr_inference(lr_img, model, scale, window_size=None):
+def run_sr_inference(lr_img, model, scale, window_size=None, tile=None, tile_overlap=None):
     """
     Run SISR inference on a low resolution image with a given model, scale, and window size (if it is the case)
     :param lr_img: LR image to be super resoluted; tensor (B, C, H, W), RGB format
     :param model: SISR model; Pytorch Module
     :param scale: raise image resolution by this factor; int
     :param window_size: size of the window used in Swin Transformer Block (for SwinIR and Swin2SR)
+    :param tile: Use this in case of CUDA out of memory. Will run the SISR model tile-by-tile. This argument
+    specifies the tile size; int
+    :param tile_overlap: How much the tiles should overlap in case we do tile-by-tile inference; int
     :return: super resoluted image; tensor (B, C, 4 * H, 4 * W), RGB format
     """
+    # We must have both "tile" and "tile_overlap" defined or neither
+    assert (tile and tile_overlap) or (tile is None and tile_overlap is None), "ERROR: Mismatch in tile definition."
+
     _, _, h_old, w_old = lr_img.shape
     h_pad = 0 if window_size is None else (window_size - h_old % window_size) % window_size
     w_pad = 0 if window_size is None else (window_size - w_old % window_size) % window_size
@@ -67,12 +77,35 @@ def run_sr_inference(lr_img, model, scale, window_size=None):
     lr_img = torch.cat([lr_img, torch.flip(lr_img, [2])], 2)[:, :, :h_old + h_pad, :]
     lr_img = torch.cat([lr_img, torch.flip(lr_img, [3])], 3)[:, :, :, :w_old + w_pad]
 
-    sr_img = model(lr_img)
+    if tile is None:
+        sr_img = model(lr_img)
+    else:
+        b, c, h, w = lr_img.shape
+        tile = min(tile, h, w)
+        assert tile is None or window_size is None or tile % window_size == 0, \
+            "ERROR: Tile should be a multiple of window size."
+
+        stride = tile - tile_overlap
+        h_idx_list = list(range(0, h - tile, stride)) + [h - tile]
+        w_idx_list = list(range(0, w - tile, stride)) + [w - tile]
+        e = torch.zeros(b, c, h * scale, w * scale).type_as(lr_img)
+        w = torch.zeros_like(e)
+
+        for h_idx in h_idx_list:
+            for w_idx in w_idx_list:
+                in_patch = lr_img[..., h_idx:h_idx + tile, w_idx:w_idx + tile]
+                out_patch = model(in_patch)
+                out_patch_mask = torch.ones_like(out_patch)
+
+                e[..., h_idx * scale:(h_idx + tile) * scale, w_idx * scale:(w_idx + tile) * scale].add_(out_patch)
+                w[..., h_idx * scale:(h_idx + tile) * scale, w_idx * scale:(w_idx + tile) * scale].add_(out_patch_mask)
+        sr_img = e.div_(w)
+
     sr_img = sr_img[..., :h_old * scale, :w_old * scale]
     return sr_img
 
 
-def sisr_forward(img, device, sisr_model, scale, window_size):
+def sisr_forward(img, device, sisr_model, scale, window_size, tile, tile_overlap):
     """
     Apply image super-resolution on given image(s) as a numpy array(s)
     :param img: image for super-resolution; np array ((B), H, W, C), RGB format
@@ -80,10 +113,18 @@ def sisr_forward(img, device, sisr_model, scale, window_size):
     :param sisr_model: SISR model; PyTorch module
     :param scale: SISR scale
     :param window_size: size of window used in Swin Transformer Blocks (for SwinIR and Swin2SR)
+    :param tile: Use this in case of CUDA out of memory. Will run the SISR model tile-by-tile. This argument
+    specifies the tile size; int
+    :param tile_overlap: How much the tiles should overlap in case we do tile-by-tile inference; int
     :return: super resoluted images; np array(s) ((B), H * 4, W * 4, C), RGB format
     """
     img_tensor = np_rgb_uint_2_tensor_norm(img, device=device)
-    img_sr_tensor = run_sr_inference(lr_img=img_tensor, model=sisr_model, scale=scale, window_size=window_size)
+    img_sr_tensor = run_sr_inference(lr_img=img_tensor,
+                                     model=sisr_model,
+                                     scale=scale,
+                                     window_size=window_size,
+                                     tile=tile,
+                                     tile_overlap=tile_overlap)
     sr_img = tensor_norm_2_np_rgb_uint(img_sr_tensor)
     return sr_img
 
@@ -111,15 +152,19 @@ def load_model(model_name, device=DEVICE, eval_model=True):
     return model
 
 
-def get_sisr_forward_fn(model_name, device=DEVICE):
+def get_sisr_forward_fn(model_name, device=DEVICE, use_tiling=False):
     """
     Get a partial function corresponding to SISR inference on a given model. Can be used as a simple
     method which is applied directly on a numpy image (or batch of numpy images).
     Available options: ["swin_ir", "swin2_sr", "esrgan", "real_esrgan", "bsrgan"]
+    If you get CUDA out of memory errors, set "use_tiling" to True so that the inference is run on tiles
+    of the image.
     """
     model = load_model(model_name=model_name, device=device, eval_model=True)
     return partial(sisr_forward,
                    device=device,
                    sisr_model=model,
                    scale=SCALE,
-                   window_size=SWIN_WINDOW_SIZE if "swin" in model_name else None)
+                   window_size=SWIN_WINDOW_SIZE if "swin" in model_name else None,
+                   tile=TILE_SIZE if use_tiling else None,
+                   tile_overlap=TILE_OVERLAP if use_tiling else None)
